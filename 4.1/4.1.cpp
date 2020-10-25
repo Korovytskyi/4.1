c4a33d54a0b5691f5f768c320a1a08da734775a2
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2020-10-25 13:09:06 +0200

    4.1

---
+++ 4.1/4.1.cpp
@@ -0,0 +1,41 @@
+﻿// 4.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, n;
+	double P;
+	cout << "n = "; cin >> n;
+	cout << "N = "; cin >> N;
+	P=1 ;
+	k=n ;
+	while (k<=25)
+	{
+		P *= sin(sqrt(1+(N/k)));
+		k++;
+	}
+	cout << P << endl;
+	P = 1;
+	k=n;
+	do {
+		P *= sin(sqrt(1 + (N / k)));
+		k++;
+	} while (k <= 25);
+	cout << P << endl;
+	P = 1;
+	for (k = n; k <= 25; k++)
+	{
+		P *= sin(sqrt(1 + (N / k)));
+	}
+	cout << P << endl;
+	P = 1;
+	for (k = 25; k >= 25; k--)
+	{
+		P *= sin(sqrt(1 + (N / k)));
+	}
+	cout << P << endl;
+	return 0;
+}
